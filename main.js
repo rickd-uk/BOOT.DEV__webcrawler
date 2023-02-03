73ee79ded547787cc302e78c91bafe5cdc2c5f96
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-02-04 00:05:15 +0900

    add - simple check for cmdline args
    Add guard statements to block arg length < 3 (which means no text has been entered after cmd)
    and arg length >3 (which means more than 1 text string has been entered after cmd)

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+
+
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log('no website')
+        process.exit(1)
+    }
+    if (process.argv.length > 3) {
+        console.log('too many args')
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl for ${baseURL}`)
+}
+
+main()
