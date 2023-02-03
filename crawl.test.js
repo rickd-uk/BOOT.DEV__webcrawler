003bffbb1fd4ef7d1cab541a3fc161bac28bb900
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-02-03 16:49:42 +0900

    add - test for normalizeURL strip protocol

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,10 @@
 import { normalizeURL } from './crawl.js';
 import { test, expect } from '@jest/globals'
 
-test('normalizeURL', () => {
-    const input = ''
+test('normalizeURL strip protocol', () => {
+    const input = 'https://bbc.com/path'
     const actual = normalizeURL(input)
-    const expected = 'fdsf'
+    const expected = 'bbc.com/path'
     expect(actual).toEqual(expected)
 })
+
