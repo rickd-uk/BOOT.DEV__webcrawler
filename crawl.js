3ae73f88b958c78761b5b987152864ae9331157e
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-02-04 12:02:44 +0900

    add - crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,28 +1,57 @@
 import { JSDOM } from 'jsdom'
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+
+
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+
+    // don't proceed if the page is not equal to the base hostname
+    // i.e. don't crawl outside the website
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        console.log(pages)
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
     console.log(`Actively crawling: ${currentURL}`)
+
 
     try {
         const res = await fetch(currentURL)
 
         if (res.status > 399) {
             console.log(`error in fetch with status code ${res.status} on page ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = res.headers.get("content-type")
 
         if (!contentType.includes('text/html')) {
             console.log(`not a html response ${contentType} on page ${currentURL}`)
-            return
+            return pages
+        }
+
+        const htmlBody = await res.text()
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
         }
 
 
-        console.log(await res.text())
+
     } catch (err) {
         console.log(`error fetching webpage: ${err.message} on ${currentURL}`)
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
